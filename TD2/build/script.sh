573aa2e1fe4d5e336cf6e5562f70724a6a610480
Lisa Taldir <145778985+lisataldir@users.noreply.github.com>
2023-12-09 19:33:13 +0100

    Delete TD2/build directory

--- TD2/build/script.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-echo " # Tranfert de u_n_0.dat, u_np1_20.dat, u_ref_20.dat u_np1_2nd_order_20.dat dans Resultats"
-mv u_n_0.dat u_np1_20.dat u_ref_20.dat u_np1_2nd_order_20.dat ../Resultats
-
-rm -rf *.dat
